fbb2afdc344d9eb78a86fbc6d9e54a06602a536c
IvanPsurtcev <87025698+IvanPsurtcev@users.noreply.github.com>
2022-08-04 23:04:52 +0300

    have some errors

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,31 @@
+use std::fs::File; // для работы с файлами
+use std::io::prelude::*; // множество типажей для работы с файлами
+use std::error::Error;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Ok(Config { query, filename })
+    }
+}
